0ae0776194cb489e242abe616cd27726256db0e1
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-15 18:51:17 -0500

    testing nginx configuration

--- hosts/dev01/default.nix
+++ hosts/dev01/default.nix
@@ -15,7 +15,13 @@
     nginx
   ];
 
-  services.nginx.enable = true;
+  services.nginx = {
+    enable = true;
+    
+    virtualHosts "/" = {
+        root = "/var/www/browser";
+    }
+  };
   
   system.stateVersion = "23.11"; # Did you read the comment?
 
