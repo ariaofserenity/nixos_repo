6004fc891eb12f35bfcf0a0e917b3f48bb385e8e
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-15 18:33:47 -0500

    test lol

--- hosts/dev01/default.nix
+++ hosts/dev01/default.nix
@@ -11,9 +11,10 @@
   networking.networkmanager.enable = true;
 
   environment.systemPackages = with pkgs; [
+    git
     nginx
   ]
-  ++ basePkgs;
+
   
   system.stateVersion = "23.11"; # Did you read the comment?
 
