15296b2bd8834777309afc4f6547e0239a1f8a1b
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-15 18:20:52 -0500

    testing multiple environment package declarations

--- hosts/web01/default.nix
+++ hosts/web01/default.nix
@@ -9,12 +9,7 @@
 
   networking.hostName = "web01";
   networking.networkmanager.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    httpd
-  ];
   
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
-
