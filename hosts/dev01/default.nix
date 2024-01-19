455e8d023bb3dfffeaaf05a70e999ed8a723170c
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-19 10:49:53 -0500

    extra mount options added

--- hosts/dev01/default.nix
+++ hosts/dev01/default.nix
@@ -27,6 +27,7 @@
   fileSystems."/mnt/common" = {
     device = "192.168.2.14:/mnt/d01/common";
     fsType = "nfs";
+    options = ["x-systemd.automount" "noauto" ];
   };
 
   system.stateVersion = "23.11"; # Did you read the comment?
