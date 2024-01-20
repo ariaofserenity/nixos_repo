54343335679aee56eaa262d5b2e1bd2ccc6aa822
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-20 13:51:00 -0500

    testing k3s

--- hosts/dev01/default.nix
+++ hosts/dev01/default.nix
@@ -9,17 +9,25 @@
 
   networking.hostName = "dev01";
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [ 80 443 22 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 22 6443 ];
+  networking.firewall.allowedUDPPorts = [ 8472 ];
   
   environment.systemPackages = with pkgs; [
     killall
   ];
+
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+  
+  environment.systemPackages = [ pkgs.k3s ];
 
   fileSystems."/mnt/common" = {
     device = "192.168.2.14:/mnt/d01/common";
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" ];
   };
+
+
 
   system.stateVersion = "23.11"; # Did you read the comment?
 
