c141b965387ba21f757004dbfa7fe4ea9da7f012
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-20 14:27:11 -0500

    bunch of various changes

--- hosts/dev02/default.nix
+++ hosts/dev02/default.nix
@@ -3,13 +3,13 @@
 {
   imports =
     [ 
-      ../../hardware-configs/dev01-hardware.nix
+      ../../hardware-configs/dev02-hardware.nix
       ./../../modules/users.nix
-      ./../../modules/k3s/server.nix
+      ./../../modules/k3s/agent.nix
       ./../../modules/nfs/client.nix
     ];
 
-  networking.hostName = "dev01";
+  networking.hostName = "dev02";
   networking.networkmanager.enable = true;
   
   environment.systemPackages = with pkgs; [
---
+++ modules/k3s/agent.nix
@@ -0,0 +1,10 @@
+{config, lib, pkgs, ...}:
+
+{
+  networking.firewall.allowedTCPPorts = [ 6443 ];
+  networking.firewall.allowedUDPPorts = [ 8472 ];
+
+  environment.systemPackages = with pkgs; [ k3s ];
+
+  services.k3s.enable = true;
+}
