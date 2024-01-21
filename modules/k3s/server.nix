b2c128692cc21798bc06f8f5ad0916e85a83c99f
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-21 16:17:58 -0500

    removed secrets from k3s config

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -5,10 +5,6 @@
   networking.firewall.allowedUDPPorts = [ 8472 ];
 
   environment.systemPackages = with pkgs; [ k3s ];
-  
-  sops.secrets.k3s_server_token = {
-    sopsFile = ./secrets.yaml;
-  };
 
   services.k3s = {
     enable = true;
