7563543cb74b0d60eaefb083ad8552745a9b2585
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-01-20 22:32:15 -0500

    reverting sops config

--- modules/k3s/server.nix
+++ modules/k3s/server.nix
@@ -5,14 +5,6 @@
   networking.firewall.allowedUDPPorts = [ 8472 ];
 
   environment.systemPackages = with pkgs; [ k3s ];
-
-  sops.secrets.k3s_token = {
-    sopsFile = ./secrets.yaml;
-  };
-  
-  sops = {
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  };
   
     services.k3s = {
     enable = true;
