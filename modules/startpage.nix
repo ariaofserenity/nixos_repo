253bcee0d11e145fb976943aad84a8a6b60f3c30
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-02-02 17:58:58 -0500

    fixed syntax error

--- modules/startpage.nix
+++ modules/startpage.nix
@@ -1,10 +1,10 @@
 {config, lib, pkgs, ...}:
 
 {
-
+    networking.firewall.allowedTCPPorts = [ 80 ];
     services.nginx.enable = true;
 
     services.nginx.virtualhosts."192.168.2.156" = {
         root = "/var/www/html";
-    }
+    };
 }
