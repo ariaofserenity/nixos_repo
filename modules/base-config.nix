8757bd347d813d1de4c1465621d7c3d6b4d66baa
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-03-02 14:49:41 -0500

    enable ssh root login

--- modules/base-config.nix
+++ modules/base-config.nix
@@ -16,12 +16,13 @@
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
+    permitRootLogin = "yes";
 
       hostKeys = [{
       path = "/etc/ssh/ssh_host_ed25519_key";
       type = "ed25519";
     }];
-    
+
     security.pam.enableSSHAgentAuth = true;
   };
   # networking.firewall.allowedUDPPorts = [ ... ];
