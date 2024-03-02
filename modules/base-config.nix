90eab350b6ad732fb2d0a3e7458896d1d0b070e5
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-03-02 14:55:07 -0500

    renaming permitrootlogin

--- modules/base-config.nix
+++ modules/base-config.nix
@@ -16,7 +16,7 @@
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
-    permitRootLogin = "yes";
+    settings.permitRootLogin = "yes";
 
       hostKeys = [{
       path = "/etc/ssh/ssh_host_ed25519_key";
