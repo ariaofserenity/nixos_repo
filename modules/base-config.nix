fa0a80e052f442061a515e3eed8024ee52ba9902
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-03-02 14:49:13 -0500

    sshd

--- modules/base-config.nix
+++ modules/base-config.nix
@@ -16,6 +16,13 @@
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
+
+      hostKeys = [{
+      path = "/etc/ssh/ssh_host_ed25519_key";
+      type = "ed25519";
+    }];
+    
+    security.pam.enableSSHAgentAuth = true;
   };
   # networking.firewall.allowedUDPPorts = [ ... ];
   
