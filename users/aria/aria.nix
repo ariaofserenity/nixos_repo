e2ff41a3ef75986ceeb7bff3feb961f4d24c3ed0
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-03-02 16:14:13 -0500

    changed ssh pubkey

--- users/aria/aria.nix
+++ users/aria/aria.nix
@@ -8,7 +8,7 @@
      extraGroups = [ "wheel" "docker" ]; 
      openssh.authorizedKeys.keys = [ 
    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDaGzAqH/9U43Bcvj9XrIWH6urY8o6GzHxBrrQOkoPyuTqPxNQT623O8bCO4SvjaC3Ajv2CLSxDFoLkoxTNPx6YrHTNtNTJOUJT8HkhnxfZKrWRmOt4Xd9agLVbluxGrlgLasfjVF5c1JyWu1/QHh72xqki6mBCfaJMtYQJHH6mNKqtUr6OBPvtwEEd5DwNIDc/hUURTG8BvqGvt/voxmTxmNOnvf556PmHCrhNyPQ4w/bMhTVQnH8Yyj+hFKgVhpT5xg5C/YFicOgQJMFg0BpOBFXXahx5+LhFwSeSFxfNyRjYO78xMLpIKg+uEcc1vOPAn9Rp/6PSzWTztR87RLG89VBtnPjlzvuWDK4it7cUiYLFFMDJGxFw9hqRSkkv6MHN85mrGCtoHVRRMjHozKEfhzhwfQo/QpbiF/VKXy8SzkhQt7s9ZAMqhfwJkJsDPB8haeN3TJ3GfPjrKL+6R/iYFIvnV6gjFeKD45LEUc1TgZNtHlRiUNlrGot5FRz9NLk= aria@zemuria"
-   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOIQ3932pPoso0lUcQxJTXNmRPGzrvUATWifoNiUUh43 aria@elysium"
+   "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKcKrN9QYfMwtym9GQoQGb/zS/HNUUpnjyrx1wPttSMn aria@elysium"
    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEZQxe+XmdZx4qleCvcOlEMEWQsbbsxz7W6M++GOk9q5 aria@valimar" ];  
      packages = with pkgs; [
        vim
