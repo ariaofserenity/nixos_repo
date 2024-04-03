fdb591ee4bedd7c765edad5b18150a471a45d9b0
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-04-02 22:13:27 -0400

    added ordine to flake

--- flake.nix
+++ flake.nix
@@ -59,6 +59,24 @@
         }
        ]; 
       };
+
+      "ordine" = nixpkgs.lib.nixosSystem {
+         inherit system;
+         specialArgs = {inherit user;};
+         modules = [ ./hosts/ordine
+         ./modules/base-config.nix
+         sops-nix.nixosModules.sops
+
+	    home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = {inherit user;};
+            home-manager.users.${user} = import ./modules/home.nix;
+        }
+       ]; 
+      };
     };
   };
 }
