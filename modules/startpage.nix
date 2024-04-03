a5a9c7477ac29f43c06ed4764a5a1e61aa4b7a28
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-04-03 14:36:41 -0400

    editing startpage

--- modules/startpage.nix
+++ modules/startpage.nix
@@ -5,7 +5,6 @@
     services.nginx.enable = true;
 
     services.nginx.virtualHosts."startpage.zemuria.lan" = {
-        #root = "/var/www/html";
-        locations."/var/www/html/index.html".proxyPass = "http://192.168.2.204/";
+        root = "/var/www/html";
     };
 }
