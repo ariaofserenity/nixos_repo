0540a27bb2b6c53197175842eb417821cb9370d2
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-04-03 11:50:59 -0400

    quick dirty deploy script

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Quick brainless script to deploy MBR NixOS hosts because im lazy
+
+host = $1
+
+create_partition_table {
+    parted /dev/sda -- mklabel msdos
+    parted /dev/sda -- mkpart primary 1MB -8GB
+    parted /dev/sda -- set 1 boot on
+    parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+}
+
+format_partitions {
+    mkfs.ext4 -L nixos /dev/sda1
+    mkswap -L swap /dev/sda2
+}
+
+mount_fs {
+    mount /dev/disk/by-label/nixos /mnt
+    swapon /dev/sda2
+}
+
+generate_config {
+    nixos-generate-config --root /mnt
+    echo 'boot.loader.grub.device = "/dev/sda";' >> /mnt/etc/nixos/configuration.nix
+    nixos-install
+}
