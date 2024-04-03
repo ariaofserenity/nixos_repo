b8c7a1523ce47057d6700b2bcea8f33962aba556
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-04-03 11:57:09 -0400

    syntax error

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Quick brainless script to deploy MBR NixOS hosts because im lazy
-start {
+start() {
     create_partition_table
 }
 
-create_partition_table {
+create_partition_table() {
     parted /dev/sda -- mklabel msdos
     parted /dev/sda -- mkpart primary 1MB -8GB
     parted /dev/sda -- set 1 boot on
@@ -13,19 +13,19 @@ create_partition_table {
     format_partitions
 }
 
-format_partitions {
+format_partitions() {
     mkfs.ext4 -L nixos /dev/sda1
     mkswap -L swap /dev/sda2
     mount_fs
 }
 
-mount_fs {
+mount_fs() {
     mount /dev/disk/by-label/nixos /mnt
     swapon /dev/sda2
     generate_config
 }
 
-generate_config {
+generate_config() {
     nixos-generate-config --root /mnt
     echo 'boot.loader.grub.device = "/dev/sda";' >> /mnt/etc/nixos/configuration.nix
     nixos-install
