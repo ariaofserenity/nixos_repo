e058827b8ae38d1165b9ca7b6e0b13e8be243d5f
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-04-03 11:53:16 -0400

    added start func

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 # Quick brainless script to deploy MBR NixOS hosts because im lazy
+start {
+    create_partition_table
+}
 
 create_partition_table {
     parted /dev/sda -- mklabel msdos
     parted /dev/sda -- mkpart primary 1MB -8GB
     parted /dev/sda -- set 1 boot on
     parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+    format_partitions
 }
 
 format_partitions {
     mkfs.ext4 -L nixos /dev/sda1
     mkswap -L swap /dev/sda2
+    mount_fs
 }
 
 mount_fs {
     mount /dev/disk/by-label/nixos /mnt
     swapon /dev/sda2
+    generate_config
 }
 
 generate_config {
@@ -24,3 +30,5 @@ generate_config {
     echo 'boot.loader.grub.device = "/dev/sda";' >> /mnt/etc/nixos/configuration.nix
     nixos-install
 }
+
+start
