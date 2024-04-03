11f65f713796d49ecefb3808cb58b36752d1b705
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-04-03 14:32:32 -0400

    testing startpage proxy

--- modules/startpage.nix
+++ modules/startpage.nix
@@ -5,7 +5,7 @@
     services.nginx.enable = true;
 
     services.nginx.virtualHosts."startpage.zemuria.lan" = {
-        root = "/var/www/html";
-        proxyPass = "http://192.168.2.202/";
+        #root = "/var/www/html";
+        locations."/var/www/html".proxyPass = "http://192.168.2.204/";
     };
 }
