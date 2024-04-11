aaf69763728503383d3d3acdee079721810a094c
aria <2664476+ariaofserenity@users.noreply.github.com>
2024-04-10 23:50:43 -0400

    added media mount

--- modules/nfs.nix
+++ modules/nfs.nix
@@ -8,4 +8,10 @@
     options = ["x-systemd.automount" "noauto" ];
   };
 
+      fileSystems."/mnt/media" = {
+    device = "192.168.2.14:/mnt/d01/media";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto" ];
+  };
+
 }
